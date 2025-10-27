5a0667f77bab8874774cab534f3d694c3c6bb2bc
pakizxx <pakizxx@gmail.com>
2025-10-27 19:41:17 +0100

    Create premake5.lua

---
+++ premake5.lua
@@ -0,0 +1,33 @@
+project "ImGui"
+    kind "StaticLib"
+    language "C++"
+    
+    targetdir ("bin/" .. outputdir .. "/")
+    objdir ("bin-int/" .. outputdir .. "/")
+
+    files
+    {
+        "imconfig.h",
+        "imgui.h",
+        "imgui.cpp",
+        "imgui_draw.cpp",
+        "imgui_internal.h",
+        "imgui_widgets.cpp",
+        "imstb_rectpack.h",
+        "imstb_textedit.h",
+        "imstb_truetype.h",
+        "imgui_demo.cpp"
+    }
+	
+	defines
+	{
+		"IMGUI_API=__declspec(dllexport)"
+	}
+    
+    filter "system:windows"
+        systemversion "latest"
+        cppdialect "C++17"
+        staticruntime "On"
+        
+    filter { "system:windows", "configurations:Release" }
+        buildoptions "/MT"
